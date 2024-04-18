a1004b0146df996eaa69432473647c5a0fdc70db
Cristian Tamas <cristiantamas05@gmail.com>
2024-04-18 16:50:43 +0300

    IMI CER SCUZE AM UITAT SA DAU STASH POP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Contact from './components/Contact'
+import Discover from './components/Discover'
+import Evolution from './components/Evolution'
+
+import Hero from './components/Hero'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="bg-background-main">
+      <Hero />
+      <Discover />
+      <Evolution />
+      <Contact />
+      <svg
+        style={{ visibility: 'hidden', position: 'absolute' }}
+        width="0"
+        height="0"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+      >
+        <defs>
+          <filter id="round">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="4" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
