55eac50e1503e4bc61b1e8836526b64322e1c3af
Cristian Tamas <cristiantamas05@gmail.com>
2024-04-27 11:46:07 +0300

    Extra padding

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => (
   <>
     <div
       id="home"
-      className="bg-hero relative w-full h-[100vh] bg-cover bg-center bg-no-repeat flex items-center justify-center mt-[70px]"
+      className="bg-hero relative w-full h-[100vh] bg-cover bg-center bg-no-repeat flex items-center justify-center mt-[80px]"
     >
       <DeviceCanvas />
       <div className="absolute flex items-center right-[1rem] py-8 h-full z-0">
